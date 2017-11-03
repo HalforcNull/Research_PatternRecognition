2f11c78202a15c7a5b0ad7f6f76e6f4075ba59ef
Halforc Null <yaorunan@gmail.com>
2017-11-03 09:20:44 -0500

    current result

---
+++ Code/test2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 27 08:34:03 2017
+
+@author: Student
+"""
+
+import numpy
+
+testdata = [1,10,100,1000,1500,2500]
+
+t = numpy.array(testdata)
+
+print(t)
+
+
+
+print(numpy.log(t))
+    
+    
+    
+    
+    
