60ff37f4fdf990a3e5a5f1b8ebe4208f8eac01bd
HalforcNull <yaorunan@gmail.com>
2017-11-13 17:07:50 -0600

    data stat code
    init

---
+++ Code/dataStat.py
@@ -0,0 +1,32 @@
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+import itertools
+import csv
+import pickle
+
+DataSet = {}
+
+def __loadData(dataFile):
+    data = []
+    with open(dataFile, 'rt') as csvfile:
+        datas = csv.reader(csvfile, delimiter = ',')
+        for row in datas:
+            if row is None or len(row) == 0:
+                continue
+            data.append(row)
+    return data
+
+log = open('log-stat.txt', 'w')
+DataList = __loadData('gtex_data.csv')
+LabelList = __loadData('label.csv')
+
+sumList = np.sum(DataList, axis=1)
+
+meanList = np.mean(np.array(DataList).astype(np.float))
+
+with open('stat-mean.csv', 'w') as csvFile:
+    wr = csv.writer(csvFile)
+    wr.writerow(meanList)
+    
+
+
