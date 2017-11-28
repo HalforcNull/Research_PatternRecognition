4f3cf9807f40d9255f56a7b41d5f70a0ce9b2ee1
HalforcNull <yaorunan@gmail.com>
2017-11-28 16:40:38 -0600

    Try seperate the loading file part and processing part

--- Code/gtex_ExceptOne_NormalizedTraining_ExportPkl.py
+++ Code/gtex_ExceptOne_NormalizedTraining_ExportPkl.py
@@ -5,8 +5,6 @@ import csv
 import pickle
 import psutil
 
-DataSet = {}
-
 def __loadData(dataFile):
     data = []
     with open(dataFile, 'rt') as csvfile:
@@ -26,55 +24,66 @@ def normalization(sample):
 def getMemoUsage():
     return str(psutil.virtual_memory().percent)
 
+def fileLoad(log):
+    DataSet = {}
+    DataList = __loadData('gtex_data.csv')
+    LabelList = __loadData('gtex_label.csv')
+    
+    log.write('File load Finished. \n')
+    log.write('Memo usage: '+ getMemoUsage() +'\n')
+    
+    
+    for i in range(0, 9662):
+        lb = LabelList[i][0]
+        if lb in DataSet.keys():
+            DataSet[lb].append(DataList[i])
+        else:
+            DataSet[lb] = [DataList[i]]
+    log.write('Data Load Finished.\n')
+    log.write('Memo usage: '+ getMemoUsage() +'\n')
+    return DataSet;
+
 log = open('excludeOnelog.txt', 'w')
 log.write('Program Start \n')
 log.write('Memo usage: '+ getMemoUsage() +'\n')
 
-DataList = __loadData('gtex_data.csv')
-LabelList = __loadData('gtex_label.csv')
 
-log.write('File load Finished. \n')
+DataSet = fileLoad(log)
+log.write('Data Pass Finished.\n')
+log.write('Memo usage: '+ getMemoUsage() +'\n')
+
+with open('./normalizedmodel/gtex/pickledRawData/Gtex.pkl', 'wb') as tr:
+    pickle.dump(DataSet, tr, pickle.HIGHEST_PROTOCOL)
+
+log.write('Data Save Finished.\n')
 log.write('Memo usage: '+ getMemoUsage() +'\n')
 
 
-for i in range(0, 9662):
-    lb = LabelList[i][0]
-    if lb in DataSet.keys():
-        DataSet[lb].append(DataList[i])
-    else:
-        DataSet[lb] = [DataList[i]]
-
-del DataList
-del LabelList
-
-log.write('Data Load Finished.\n')
-log.write('Memo usage: '+ getMemoUsage() +'\n')
-
-labels = DataSet.keys()
-
-for i in range( 0, 10 ):
-    l = labels[i]
-    model = GaussianNB() 
-    log.write('Building pickle for: ' + l + ' excluded.' )
-    Data = []
-    Label = []
-    for j in labels:
-        if l != j:
-            Data = Data + DataSet[j]
-            Label = Label + [j] * len(DataSet[j])
-            
-    testTraining = np.array(Data).astype(np.float)
-    # do normalization here
-    testTraining = np.apply_along_axis(normalization, 1, testTraining )
-    testlabeling = np.array(Label)
-    model.fit(testTraining,testlabeling)
-    
-    log.write('Model: ' + l + ' training finished.\n')
-    log.write('Memo usage: '+ getMemoUsage() +'\n')
-    
-    with open('./normalizedmodel/gtex/excludeOne/'+l+'.pkl', 'wb') as tr:
-        pickle.dump(model, tr, pickle.HIGHEST_PROTOCOL)
-    
-    log.write('pickel: ' + l + ' write done.\n')
-    log.write('Memo usage: '+ getMemoUsage() +'\n')
+#labels = DataSet.keys()
+#
+#for i in range( 0, 10 ):
+#    l = labels[i]
+#    model = GaussianNB() 
+#    log.write('Building pickle for: ' + l + ' excluded.' )
+#    Data = []
+#    Label = []
+#    for j in labels:
+#        if l != j:
+#            Data = Data + DataSet[j]
+#            Label = Label + [j] * len(DataSet[j])
+#            
+#    testTraining = np.array(Data).astype(np.float)
+#    # do normalization here
+#    testTraining = np.apply_along_axis(normalization, 1, testTraining )
+#    testlabeling = np.array(Label)
+#    model.fit(testTraining,testlabeling)
+#    
+#    log.write('Model: ' + l + ' training finished.\n')
+#    log.write('Memo usage: '+ getMemoUsage() +'\n')
+#    
+#    with open('./normalizedmodel/gtex/excludeOne/'+l+'.pkl', 'wb') as tr:
+#        pickle.dump(model, tr, pickle.HIGHEST_PROTOCOL)
+#    
+#    log.write('pickel: ' + l + ' write done.\n')
+#    log.write('Memo usage: '+ getMemoUsage() +'\n')
 
