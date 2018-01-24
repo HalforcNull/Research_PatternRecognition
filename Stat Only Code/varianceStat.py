0bb1603067a613620c25be2434bd86075cc668c7
HalforcNull <yaorunan@gmail.com>
2018-01-24 15:42:45 -0600

    add more gtex and tcga compare

--- Stat Only Code/varianceStat.py
+++ Stat Only Code/varianceStat.py
@@ -18,8 +18,33 @@ def __loadData(dataFile, isNumericData = False):
                 data.append(row)
     return data
 
+def normalization(sample):
+    """one sample pass in"""
+    sample = sample + 100
+    # 2^20 = 1048576
+    return np.log2(sample * 1048576/np.sum(sample))
 
-def topGeneCalc( name ,dataF, labelF, opDataF, logSys):
+
+def outputDataS(name, DataS, logSys):
+    myvar = np.var(DataS, axis = 0)
+    myav = np.mean(DataS, axis = 0)
+    myavsq = np.multiply(myav, myav)
+    myr = np.divide(myvar, myavsq)                      # variance / average^2
+
+    # with open(opDataF, 'w') as csvFile:
+    #     wr = csv.writer(csvFile)
+    #     wr.writerow(myr)
+
+    s = np.argsort(myr)                                 # sort and get the index
+    s = s[::-1]
+
+    #myr[s[120]]                                         # gtex, 120 gene is enough to 
+    logSys.write( 'top 120 of ' + name + ' is: ' + str(s[1:120]))
+    logSys.write( 'the last one is: ' + str(myr[s[120]])) 
+    return s[1:120]
+
+
+def topGeneCalc( name ,dataF, labelF, opDataF, procType, logSys):
     DataList = __loadData(dataF, isNumericData = True)
     LabelList = __loadData(labelF)
 
@@ -38,30 +63,41 @@ def topGeneCalc( name ,dataF, labelF, opDataF, logSys):
             LabelCount[lb] = 0
 
     logSys.write('Data Load Finished.')
-    myvar = np.var(DataS, axis = 0)
-    myav = np.mean(DataS, axis = 0)
-    myavsq = np.multiply(myav, myav)
-    myr = np.divide(myvar, myavsq)                      # variance / average^2
 
-    with open(opDataF, 'w') as csvFile:
-        wr = csv.writer(csvFile)
-        wr.writerow(myr)
-
-    s = np.argsort(myr)                                 # sort and get the index
-    s = s[::-1]
-
-    #myr[s[120]]                                         # gtex, 120 gene is enough to 
-    logSys.write( 'top 120 of ' + name + ' is: ' + str(s[1:120]))
-    logSys.write( 'the last one is: ' + str(myr[s[120]])) 
-    return s[1:120]
+    if procType == 1:
+        return outputDataS("orginal " + name, DataS, logSys)       # original
+    elif procType == 2:                 
+        DS = np.array(DataSet).astype(np.float)      #np array
+        return outputDataS("np array" + name, DS, logSys)
+    else:
+        DS = np.array(DataSet).astype(np.float)
+        return outputDataS("normalized" + name, DS, normalization(DS))
+    
 
 
 log = open('consoleTest.txt', 'a')
-gtex = topGeneCalc('gtex', 'gtex_data.csv', 'gtex_label.csv', 'gtex.detailed.result.csv', log)
-tcga = topGeneCalc('tcga', 'tcga_data.csv', 'tcga_label.csv', 'tcga.detailed.result.csv', log)
+gtex1 = topGeneCalc('gtex', 'gtex_data.csv', 'gtex_label.csv', 'gtex.detailed.result.csv', 1, log)
+tcga1 = topGeneCalc('tcga', 'tcga_data.csv', 'tcga_label.csv', 'tcga.detailed.result.csv', 1, log)
 
-BothGtexTcga = set(gtex) & set(tcga)
-EitherGtexTcga = set(gtex) | set(tcga)
+gtex2 = topGeneCalc('gtex', 'gtex_data.csv', 'gtex_label.csv', 'gtex.detailed.result.csv', 2, log)
+tcga2 = topGeneCalc('tcga', 'tcga_data.csv', 'tcga_label.csv', 'tcga.detailed.result.csv', 2, log)
 
-log.write( str(len(BothGtexTcga)) + ' genes in both, they are: ' + str(BothGtexTcga))
-log.write( str(len(EitherGtexTcga)) + ' genes in either, they are: ' + str(EitherGtexTcga))
+gtex3 = topGeneCalc('gtex', 'gtex_data.csv', 'gtex_label.csv', 'gtex.detailed.result.csv', 3, log)
+tcga3 = topGeneCalc('tcga', 'tcga_data.csv', 'tcga_label.csv', 'tcga.detailed.result.csv', 3, log)
+
+BothGtex = set(gtex1) & set(gtex2) & set(gtex3)
+BothTcga = set(tcga1) & set(tcga2) & set(tcga3)
+
+log.write( '\r\n' )
+log.write( str(len(BothGtex)) + ' genes in all gtex, they are: ' + str(BothGtex))
+log.write( '\r\n' )
+log.write( str(len(BothTcga)) + ' genes in all tcga, they are: ' + str(BothTcga))
+
+
+
+# BothGtexTcga1 = set(gtex1) & set(tcga1)
+# EitherGtexTcga1 = set(gtex1) | set(tcga1)
+# log.write( '\r\n' )
+# log.write( str(len(BothGtexTcga)) + ' genes in both, they are: ' + str(BothGtexTcga))
+# log.write( '\r\n' )
+# log.write( str(len(EitherGtexTcga)) + ' genes in either, they are: ' + str(EitherGtexTcga))
